491ecba1990d30e7446162efb8b699ca33ca2493
Kai KRETSCHMANN <kai@kretschmann.consulting>
2021-02-10 18:02:56 +0100

    Add test step

---
+++ test/api_count.js
@@ -0,0 +1,36 @@
+/**
+ * Test API
+ * @see DDATA-functional-API-numbers
+ */
+
+var chai = require('chai');
+var expect = chai.expect;
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+chai.use(require('chai-json-schema'));
+
+const service = require('../service/PackagesService.js');
+
+describe('getCount', function() {
+
+	context('[DDATA-5] with right arguments', function() {
+		it('[STEP-1] should generate number property', async () => {
+			const countSchema = {
+				title: 'count schema',
+				type: 'object',
+				required: ['count'],
+				properties: {
+					count: {
+						type: 'number',
+						minimum: 1
+					}
+				}
+			};
+			var result = await service.countPackage();
+			return expect(result).to.be.jsonSchema(countSchema);
+		});
+	})
+
+});
+
---
+++ test/server_common.js
@@ -0,0 +1,25 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+let request = require('supertest');
+
+chai.use(chaiHttp);
+
+describe('Server common', () => {
+
+	describe('[DDATA-15] /GET home', () => {
+		it('[STEP-1] should redirect', (done) => {
+		  request(server)
+		      .get('/')
+		      .end((err, res) => {
+			    res.should.have.status(301);
+			    done();
+		        });
+		});
+	});
+});
