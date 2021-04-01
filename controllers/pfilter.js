acbf3c768754c8b7e116fe4f0e747c155e96d049
Kai KRETSCHMANN <kai@kretschmann.consulting>
2021-04-01 13:20:53 +0200

    Add pfilter keywords

--- controllers/pfilter.js
+++ controllers/pfilter.js
@@ -8,6 +8,14 @@ function cleanupString(s) {
 	return sNew;
 }
 
+function cleanupWord(s) {
+        var sNew = s.replace(/[^a-zA-Z]/gi, '');
+        if(sNew != s) {
+                console.warn("Filtered invalid chars");
+        }
+        return sNew;
+}
+
 function cleanupNumber(s) {
         var sNew = s.replace(/[^0-9]/gi, '');
         if(sNew != s) {
@@ -26,15 +34,17 @@ function cleanupStringHex(s) {
 
 module.exports = function (req, res, next) {
     console.log("In pfilter: " + req.url);
-    //console.log(req);
-    if(req.url == "/" || req.url.startsWith("/docs/") || req.url.startsWith("/api-docs")) {
-	    console.debug("static stuff OK");
+    if(! req.url.startsWith("/v1/")) {
+	    console.debug("Ignore non API stuff");
 	    next();
     } else {
 	    for(var [key, value] of Object.entries(req.query)) {
 		    console.debug(key + ":" + value);
               switch(key) {
                 case "count":
+                case "skip":
+                case "page":
+                case "size":
                     req.query[key] = cleanupNumber(value);
                     break;
                 case "packageName":
@@ -46,6 +56,11 @@ module.exports = function (req, res, next) {
                 case "packageHash":
                     req.query[key] = cleanupStringHex(value);
                     break;
+                case "sort":
+                case "sorters":
+                case "direction":
+                    req.query[key] = cleanupWord(value);
+                    break;
                 case "id":
                     req.query[key] = cleanupStringHex(value);
                     if(value.length != 24) {
@@ -54,6 +69,9 @@ module.exports = function (req, res, next) {
                         var response = { message: "Error: ID must be 24 hex chars" };
                         return res.end(JSON.stringify(response));
                     }
+                    break;
+                default:
+                    console.warn("Don't know about " + key);
                     break;
               }
 	    }
