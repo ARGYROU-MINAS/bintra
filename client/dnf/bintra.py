7b9c5e2812b963405f97d879d4d7d12790e6975b
Kai KRETSCHMANN <kai@kretschmann.consulting>
2021-04-24 13:06:51 +0200

    get all data and hash

--- client/dnf/bintra.py
+++ client/dnf/bintra.py
@@ -5,32 +5,54 @@
 from dnfpluginscore import _, logger
 import dnf
 import dnf.cli
+import os
+import hashlib
 from ppretty import ppretty
+from shutil import copyfile
 
 class Bintra(dnf.Plugin):
     name = 'bintra'
+    JWT = ''
 
     def __init__(self, base, cli):
         super(Bintra, self).__init__(base, cli)
         logger.info('In bintra init')
 
+    def config(self):
+        cp = self.read_config(self.base.conf)
+        self.JWT = cp.get('main', 'JWT')
+
+    @staticmethod
+    def _calcHash(filename):
+        sha256_hash = hashlib.sha256()
+        with open(filename,"rb") as f:
+            for byte_block in iter(lambda: f.read(4096),b""):
+                sha256_hash.update(byte_block)
+        return sha256_hash.hexdigest()
+
     def pre_transaction(self):
         logger.info('In bintra pretransaction')
-        print(ppretty(self.base.transaction.install_set, indent='    ', width=40, seq_length=50, show_protected=False, show_static=False, show_properties=True, show_address=False))
+        #print(ppretty(self.base.transaction.install_set, indent='    ', width=40, seq_length=50, show_protected=False, show_static=False, show_properties=True, show_address=False))
 
         _cmd = self.cli.command._basecmd
         logger.debug('Pre transaction command %s', _cmd)
+        logger.info('JWT still %s', self.JWT)
 
         if 'install' != _cmd:
             logger.info('Ignore command')
             return
 
         # loop over packages downloaded and to be installed
+        _i = 0
         for p in self.base.transaction.install_set:
+            _i = _i + 1
             _arch = p.arch
             _version = p.evr
-            _path = p.repo.pkgdir + '/' + p.relativepath
-            logger.info('Check version %s for architecture %s in temp path %s', _version, _arch, _path)
+            _family = p.vendor
+            _path = p.repo.pkgdir + '/' + os.path.basename(p.relativepath)
+            logger.info('Check version %s for architecture %s, family %s in temp path %s', _version, _arch, _family, _path)
+            _hash = self._calcHash(_path)
+            logger.info('Hash is %s', _hash)
 
         # exit method:
         raise dnf.exceptions.Error('Stop for test')
