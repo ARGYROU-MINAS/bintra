aaf639d3f1a31b44730360df4c25efdd03393b89
Kai KRETSCHMANN <kai@kretschmann.consulting>
2021-05-13 09:47:28 +0200

    Update wsus script, #21

--- client/windows/wsus_trigger.ps1
+++ client/windows/wsus_trigger.ps1
@@ -1,83 +1,86 @@
-Add-Type -AssemblyName System.Web
+﻿Add-Type -AssemblyName System.Web
 
-$log = "D:\Logs\Approved_Updates.txt"
-
+Write-Host (“Setting variables ”)
+$log = "D:\Logs\newlog.txt"
+$WsusServerFqdn=(Get-ItemProperty -path 'HKLM:\SOFTWARE\Bintra').WSUSServerName
+$WsusSourceGroup = (Get-ItemProperty -path 'HKLM:\SOFTWARE\Bintra').SourceGroupName
+$WsusTargetGroup = (Get-ItemProperty -path 'HKLM:\SOFTWARE\Bintra').TargetGroupName
 $WsusContentFolder = (Get-ItemProperty -path 'HKLM:\SOFTWARE\Bintra').WSUSContentFolder
 $JWT = (Get-ItemProperty -path 'HKLM:\SOFTWARE\Bintra').JWT
-$GroupName = (Get-ItemProperty -path 'HKLM:\SOFTWARE\Bintra').GroupName
-$WsusContentFolder | Out-File $log -append 
-$JWT
 
-$wsus = Get-WsusServer
-$wsus | Out-File $log -append 
+$WsusContentFolder | Out-File $log -append 
 
 $headers = @{
     'Authorization' = 'Bearer ' + $JWT
 }
 
-$wgroup = $wsus.GetComputerTargetGroups() | where {$_.Name -eq $GroupName}
-$wgroup
+[void][reflection.assembly]::LoadWithPartialName( “Microsoft.UpdateServices.Administration”)
+$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::getUpdateServer( $WsusServerFqdn, $False, ‘8530’)
+$Groups = $wsus.GetComputerTargetGroups()
+$WsusSourceGroupObj = $Groups | Where {$_.Name -eq $WsusSourceGroup}
+$WsusTargetGroupObj = $Groups | Where {$_.Name -eq $WsusTargetGroup}
 
-#Get all updates
-# filter perhaps this: -and ($_.ProductFamilyTitles -eq "Windows" -or $_.ProductFamilyTitles -eq "Office")
-$updates = Get-WsusUpdate -Approval Unapproved | ? {( $_.PublicationState -ne "Expired" ) }
-
-
-#Iterate every update and output some basic info about it
-$CurrentFile = 0
-ForEach ($update in $updates) {
-  $CurrentFile += 1
-  $CurrentFilePercent = 100 - ((($updates.Count - $CurrentFile) / $updates.Count) * 100)
-  "Checking $($update.Update.Title)" | Out-File $log -append 
-
-  $doApprove = $false
-  $update.Update.GetInstallableItems().Files | foreach {
-    # $_ | Out-File $log -append 
-    if ($_.Type -eq [Microsoft.UpdateServices.Administration.FileType]::SelfContained -and ($_.FileUri -match '[cab|msu]$'))
+$Updates = $wsus.GetUpdates()
+$i = 0
+ForEach ($Update in $Updates)
+{
+    if ($Update.GetUpdateApprovals($WsusSourceGroupObj).Count -ne 0 -and $Update.GetUpdateApprovals($WsusTargetGroupObj).Count -eq 0)
     {
-      $LocalName = $_.Name
-      $LocalFileName = ($_.FileUri -replace '.*/Content', $WsusContentFolder) -replace '/', '\'
-      $LocalFileName | Out-File $log -append 
-	  if (Test-Path $LocalFileName -PathType leaf)
-	  {
-        $LocalVersion = "NA" #[System.Diagnostics.FileVersionInfo]::GetVersionInfo($LocalFileName).FileVersion
-        $LocalVersion | Out-File $log -append 
+        $i ++
+        Write-Host (“Validation ” + $Update.Title)
 
-        $LocalHash = Get-FileHash $LocalFileName
-        $LocalHash.Hash | Out-File $log -append 
+        $doApprove = $false
+        $Update.GetInstallableItems().Files | foreach {
+            $_ | Out-File $log -append 
+            if (($_.Type -eq [Microsoft.UpdateServices.Administration.FileType]::SelfContained -or $_.Type -eq [Microsoft.UpdateServices.Administration.FileType]::None) -and ($_.FileUri -match '[cab|msu|exe]$'))
+            {
+                $LocalName = $_.Name
+                $LocalFileName = ($_.FileUri -replace '.*/Content', $WsusContentFolder) -replace '/', '\'
+                $LocalFileName | Out-File $log -append 
+	            if (Test-Path $LocalFileName -PathType leaf)
+	            {
+                    $LocalVersion = "NA" #[System.Diagnostics.FileVersionInfo]::GetVersionInfo($LocalFileName).FileVersion
+                    $LocalVersion | Out-File $log -append 
 
-        $LocalArch = "x86"
-        if ($LocalFileName.Contains("x64.cab") -or $LocalFileName.Contains("x64-NDP48.cab"))
-	    {
-		    $LocalArch = "x64"
-	    }
-        if ($LocalFileName.Contains("arm64.cab") -or $LocalFileName.Contains("arm64-NDP48.cab"))
-	    {
-		    $LocalArch = "arm64"
-	    }
+                    $LocalHash = Get-FileHash $LocalFileName
+                    $LocalHash.Hash | Out-File $log -append
 
-        $uri = "https://api.binarytransparency.net/v1/package?" +
+                    Write-Host (“Local name ” + $LocalName)
+                    $LocalArch = "x86"
+                    if ($LocalName.Contains("x64.") -or $LocalName.Contains("x64-NDP48."))
+	                {
+		                $LocalArch = "x64"
+            	    }
+                    if ($LocalName.Contains("arm64.") -or $LocalName.Contains("arm64-NDP48."))
+	                {
+		                $LocalArch = "arm64"
+	                }
+
+                    $uri = "https://api.binarytransparency.net/v1/package?" +
           "packageName=" + [System.Web.HttpUtility]::UrlEncode($LocalName) +
           "&packageVersion=" + [System.Web.HttpUtility]::UrlEncode($LocalVersion) +
           "&packageArch=" + $LocalArch +
           "&packageFamily=Windows" +
           "&packageHash=" + [System.Web.HttpUtility]::UrlEncode($LocalHash.Hash)
-        $uri | Out-File $log -append 
+                    $uri | Out-File $log -append 
 
-        $api = Invoke-RestMethod -Uri $uri -Method PUT -Headers $headers -UserAgent "Bintra 0.0.1 (Windows)"
-        $api | Out-File $log -append 
-	    $doApprove = $true
-	  } else {
-		"File not there" | Out-File $log -append
-	  }
-    }
-  }
-  if ($doApprove)
-  {
-	  "Will approve that update" | Out-File $log -append 
-	  $update.Approve("Install", $wgroup)
-  }
-  #break
-}
+                    $api = Invoke-RestMethod -Uri $uri -Method PUT -Headers $headers -UserAgent "Bintra 0.0.2 (Windows)"
+                    $api | Out-File $log -append 
+	                $doApprove = $true
+	            } else {
+		            "File not there" | Out-File $log -append
+	            } #else if
+            } #if
+        } # foreach inner
 
-"Ende" | Out-File $log -append
+        if ($doApprove)
+        {
+            Write-Host (“Approving ” + $Update.Title)
+            $Update.Approve(‘Install’, $WsusTargetGroupObj) | Out-File $log -append
+        } else {
+            Write-Host (“** SKIPPING ** ” + $Update.Title)
+        }
+    } # if
+} # foreach
+
+Write-Output (“Approved {0} updates for target group {1}” -f $i, $WsusTargetGroup)
