d16a5bd47af7544cb6f4aeeb39ce5d23bdad9aa6
Kai KRETSCHMANN <kai@kretschmann.consulting>
2021-07-09 11:05:38 +0200

    MQTT works for non ssl now

--- subscribers/mqttclient.js
+++ subscribers/mqttclient.js
@@ -4,26 +4,22 @@ var emitter = require('events').EventEmitter;
 var eventEmitter = require('../utils/eventer').em;
 
 var mqtt=require('mqtt');
-var client;
+var client=null;
 
-eventEmitter.on('putdata', function getPutDataHit(packageName, packageVersion, packageArch, packageFamily, packageHash, isnew) {
+// initial init steps
+console.log("In mqttclient initial init");
 
-  if(typeof process.env.MQTT_HOSTNAME === 'undefined' || process.env.MQTT_HOSTNAME === null) {
-    console.debug("No MQTT defined");
-    return;
-  }
-
+if(typeof process.env.MQTT_HOSTNAME === 'undefined' || process.env.MQTT_HOSTNAME === null) {
+  console.warn("No MQTT defined");
+} else {
   if(typeof client === 'undefined' || client === null || !client.connected) {
     console.log("MQTT do connect");
 
-    client = mqtt.connect("mqtts://" + process.env.MQTT_HOSTNAME + ":8883",
+    client = mqtt.connect("mqtt://" + process.env.MQTT_HOSTNAME,
     {
       clientId: "bintraService",
       username: process.env.MQTT_USERNAME,
       password: process.env.MQTT_PASSWORD
-    });
-    client.on("connect", function() {
-      console.log("MQTT connected");
     });
     client.on("error", function(error) {
       console.error("MQTT error " + error);
@@ -31,7 +27,9 @@ eventEmitter.on('putdata', function getPutDataHit(packageName, packageVersion, p
   } else {
     console.log("Reuse MQTT client connection");
   }
+}
 
+eventEmitter.on('putdata', function getPutDataHit(packageName, packageVersion, packageArch, packageFamily, packageHash, isnew) {
   if(!client.connected) {
     console.log("MQTT not connected, skipping publish");
     return;
