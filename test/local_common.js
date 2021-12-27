a2a5e319231e336619824f67db68d9b5719a5e79
Kai KRETSCHMANN <kai@kretschmann.consulting>
2021-12-27 16:51:15 +0100

    Trigger test

--- test/local_common.js
+++ test/local_common.js
@@ -1,5 +1,5 @@
 /**
- * Test local interface for manual admin commands, see requirements
+ * Test local interface for manual admin commands, see requirements.
  * @see DDATA-functional-API-numbers
  */
 
