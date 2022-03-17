587cc1e0eedffd68c35a6b8c07e3a14490ae3d36
Michael Kretschmann <kaikretschmann@gmail.com>
2022-03-17 13:11:22 +0000

    Make service names unique

--- setup_env_test.sh
+++ setup_env_test.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+suffix=''
+while getopts s: flag
+do
+    case "${flag}" in
+        s) suffix=${OPTARG};;
+    esac
+done
+echo "Suffix: $suffix";
+
 echo "Start writing .env.test"
 
 echo BIND_HOST=0.0.0.0 >.env.test
 echo BIND_PORT=8080 >>.env.test
-echo MONGO_URL="mongodb://mongo/bintratest?directConnection=true" >>.env.test
+echo MONGO_URL="mongodb://mongo$suffix/bintratest?directConnection=true" >>.env.test
 echo MONGO_USERNAME= >>.env.test
 echo MONGO_PASSWORD= >>.env.test
-echo MONGO_HOSTNAME=mongo >>.env.test
+echo MONGO_HOSTNAME=mongo$suffix >>.env.test
 echo MONGO_PORT=27017 >>.env.test
 echo MONGO_DB=bintratest >>.env.test
 echo JWT_SECRET=SomeSecret >>.env.test
@@ -18,11 +27,11 @@ echo MATOMO_URL=https://stat.myocastor.de/piwik.php >>.env.test
 echo MATOMO_ID=141 >>.env.test
 echo MATOMO_TOKEN_AUTH=SOMETHINGWRONG >>.env.test
 echo MQTT_PROTO=mqtt >>.env.test
-echo MQTT_HOSTNAME=mqtt >>.env.test
+echo MQTT_HOSTNAME=mqtt$suffix >>.env.test
 echo MQTT_USERNAME= >>.env.test
 echo MQTT_PASSWORD= >>.env.test
 echo SENTRY=${SENTRY_URL} >>.env.test
-echo REDIS_HOSTNAME=redis >>.env.test
+echo REDIS_HOSTNAME=redis$suffix >>.env.test
 echo BUSY_LAG=150 >>.env.test
 echo BUSY_INTERVAL=500 >>.env.test
 echo LOGLEVEL=debug >>.env.test
